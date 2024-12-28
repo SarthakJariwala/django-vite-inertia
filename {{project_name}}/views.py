81b397a239af5326f05f8482a9b130b8957be3ae
Sarthak Jariwala <sarthak@Vinushas-MacBook-Pro.local>
2024-12-27 17:40:14 -0800

    rm events - keep simple index view

--- {{project_name}}/views.py
+++ {{project_name}}/views.py
@@ -1,13 +1,5 @@
-from django.contrib import messages
 from inertia import render
 
 
 def home(request):
-    messages.success(request, "Hello from Inertia!")
-    return render(
-        request,
-        "Index",
-        props={
-            "events": ["Event 1", "Event 2"],
-        },
-    )
+    return render(request, "Index")
